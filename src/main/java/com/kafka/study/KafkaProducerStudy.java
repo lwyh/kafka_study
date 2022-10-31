8109468d559cd53327279fe452fb3440b48eb38c
lwyh <1977004847@qq.com>
2022-10-31 11:02:07 +0800

    第二次提交

--- src/main/java/com/kafka/study/KafkaProducerStudy.java
+++ src/main/java/com/kafka/study/KafkaProducerStudy.java
@@ -1,4 +1,20 @@
 package com.kafka.study;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
 public class KafkaProducerStudy {
+    public static void main(String[] args) {
+         Properties properties = new Properties();
+         properties.put("bootstrap.servers", "192.168.46.130:9092");
+         properties.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+         properties.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+         properties.put("retries",1);
+        final KafkaProducer<String,String> kafkaProducer = new KafkaProducer<>(properties);
+        final ProducerRecord<String,String> producerRecord = new ProducerRecord("mrt","sssss");
+        kafkaProducer.send(producerRecord);
+        kafkaProducer.close();
+    }
 }
